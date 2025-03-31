d751a023fbfb870fd1b6064791a6efaa5062829a
Alex Martin <aam65@student.le.ac.uk>
2025-03-31 12:08:31 +0100

    Added graceful shutdown

--- cmd/podconfig/main.go
+++ cmd/podconfig/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/Takenobou/podconfig/internal/config"
 	"github.com/Takenobou/podconfig/internal/web"
@@ -24,6 +29,27 @@ func main() {
 	http.HandleFunc("/feeds", handler.FeedListHandler)
 	http.HandleFunc("/remove", handler.RemoveFeedHandler)
 
-	log.Printf("Starting server on :%s", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	server := &http.Server{
+		Addr: ":" + port,
+	}
+
+	go func() {
+		log.Printf("Starting server on :%s", port)
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v", err)
+	}
+
+	log.Println("Server exiting")
 }
